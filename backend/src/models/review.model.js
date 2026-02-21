ef19aa0595a7ed1d9e060ea8097a1182f0a3c970
KWKArosha <aroshoffice@gmail.com>
2026-02-21 10:48:40 +0530

    fixed users error

--- backend/src/models/review.model.js
+++ backend/src/models/review.model.js
@@ -19,7 +19,7 @@ const reviewSchema = new mongoose.Schema(
     },
     user: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: "users",
+      ref: "User",
       required: true,
     },
   },
