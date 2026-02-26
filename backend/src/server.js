d081732f4926e8fc4ca7dda247d696130a1230f5
ATharushika <atharushika50@gmail.com>
2026-02-26 14:39:52 +0530

    Fix dotenv configuration import order

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,5 @@
-dotenv.config()
 import dotenv from 'dotenv';
-
+dotenv.config()
 import express from 'express';
 import notesRoutes from './routes/notesRoutes.js';  
 import {connectDB} from './config/db.js';
