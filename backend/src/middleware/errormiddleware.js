29f184370ad94233984fa6f27074157d9c71eacd
ATharushika <atharushika50@gmail.com>
2026-03-21 19:26:44 +0530

    add validations

--- backend/src/middleware/errormiddleware.js
+++ backend/src/middleware/errormiddleware.js
@@ -1,7 +1,14 @@
-//backend/src/middleware/errormiddleware.js
+// middleware/errormiddleware.js
 const errorHandler = (err, req, res, next) => {
-  res.status(res.statusCode || 500).json({
-    message: err.message,
+  console.error("ERROR:", err.message);
+
+  const statusCode = res.statusCode && res.statusCode !== 200
+    ? res.statusCode
+    : 500;
+
+  res.status(statusCode).json({
+    message: err.message || "Internal Server Error",
+    stack: process.env.NODE_ENV === "production" ? null : err.stack,
   });
 };
 
